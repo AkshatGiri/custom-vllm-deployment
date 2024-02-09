b123076463445ce474a008b06d97a919f3309946
Akshat Giri <akshatgiri@gmail.com>
2024-02-09 04:31:16 +0000

    Updating model

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 huggingface-cli login --token $HUGGINGFACE_TOKEN --add-to-git-credential
 
-huggingface-cli download TheBloke/U-Amethyst-20B-GPTQ --revision gptq-4bit-32g-actorder_True --local-dir ./model --local-dir-use-symlinks True
+huggingface-cli download TheBloke/neural-chat-7B-v3-2-GPTQ --revision gptq-4bit-32g-actorder_True --local-dir ./model --local-dir-use-symlinks True
 
-python3 -m vllm.entrypoints.openai.api_server --model ./model --host 0.0.0.0 --trust-remote-code --max-model-len 3792 --chat-template ./chat-templates/Llama-v2.jinja
+python3 -m vllm.entrypoints.openai.api_server --model ./model --host 0.0.0.0 --trust-remote-code --max-model-len 4096 --chat-template ./chat-templates/Llama-v2.jinja
