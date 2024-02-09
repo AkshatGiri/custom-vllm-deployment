b4ecd3a7af9495c961bdf0cbbea2e227db4d842f
Akshat Giri <akshatgiri@gmail.com>
2024-02-09 00:52:55 +0000

    Adding chat template

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM vllm/vllm-openai
 
 RUN pip install -U "huggingface_hub[cli]"
 
+COPY chat-templates ./chat-templates
+
 COPY run.sh .
 RUN chmod +x run.sh
+
 ENTRYPOINT ["./run.sh"]
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ huggingface-cli login --token $HUGGINGFACE_TOKEN --add-to-git-credential
 
 huggingface-cli download TheBloke/U-Amethyst-20B-GPTQ --revision gptq-4bit-32g-actorder_True --local-dir ./model --local-dir-use-symlinks True
 
-python3 -m vllm.entrypoints.openai.api_server --model ./model --host 0.0.0.0 --trust-remote-code --max-model-len 3792
+python3 -m vllm.entrypoints.openai.api_server --model ./model --host 0.0.0.0 --trust-remote-code --max-model-len 3792 --chat-template ./chat-templates/Llama-v2.jinja
